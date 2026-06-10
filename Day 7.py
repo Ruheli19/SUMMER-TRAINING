35069fc990b3e2504aa1c21cc3695a28e217f442
Ruheli <ruhelighosh@gmail.com>
2026-06-10 21:29:51 +0530

    Q4

--- Day 7.py
+++ Day 7.py
@@ -1,9 +1,7 @@
 
-#print character triangle
-n=5
+#print repeated-number pattern
+n=int(input("enter the number of rows:"))
 for i in range(1,n+1):
-    for j in range(0,i):
-        print()
-
+    print(str(i)*i)
 
     
