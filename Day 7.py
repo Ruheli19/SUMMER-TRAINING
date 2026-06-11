1c58c434f71fdb6ef895623dbf68e3dd8aca8beb
Ruheli <ruhelighosh@gmail.com>
2026-06-11 12:20:52 +0530

    Day8

--- Day 7.py
+++ Day 7.py
@@ -1,7 +1,7 @@
 
-#print repeated-number pattern
+#print reverse star pattern
 n=int(input("enter the number of rows:"))
-for i in range(1,n+1):
-    print(str(i)*i)
+for i in range(1,0,-1):
+    print("*"*i)
 
     
