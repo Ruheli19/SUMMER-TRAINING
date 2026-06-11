cb1ff5f8e554ccf8eef1760add7b1f59051ed7a7
Ruheli <ruhelighosh@gmail.com>
2026-06-11 13:08:40 +0530

    Day9 Q3

--- Day 9.py
+++ Day 9.py
@@ -1,9 +1,9 @@
 
-#print reverse pyramid
+#print number pyramid
 n=5
-for i in range (n,0,-1):
-    for j in range(n-i):
-        print("",end="")
-        for j in range(2*i-1):
-            print("*",end="")
+for i in range (1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+        for j in range(i-1,0,-1):
+            print(j,end="")
             print()
