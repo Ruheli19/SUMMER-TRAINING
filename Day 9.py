374633b604a9729ce012d1c072e2c6b81d137278
Ruheli <ruhelighosh@gmail.com>
2026-06-11 13:15:02 +0530

    Day9 Q4

--- Day 9.py
+++ Day 9.py
@@ -1,9 +1,11 @@
 
-#print number pyramid
+#print character pyramid
 n=5
-for i in range (1,n+1):
-    for j in range(1,i+1):
-        print(j,end="")
-        for j in range(i-1,0,-1):
-            print(j,end="")
+for i in range (n):
+    print(""*(n-i-1),end="")
+    for j in range(i+1):
+        print(chr(65+j),end="")
+        for j in range(i-1,-1,-1):
+            print(chr(65+j),end="")
             print()
+            
